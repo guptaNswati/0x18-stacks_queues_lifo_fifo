ef718d64d13849a7f7fb1d9f715391d149e1521a
guptaNswati <sgupta1505@gmail.com>
2017-01-22 01:41:57 +0000

    print funcs

--- opcodefunc.c
+++ opcodefunc.c
@@ -52,3 +52,47 @@ void swap_tops(stack_s **head, int num)
 	(*head)->next->n = tmp;
 }
 
+/**
+* chr_top - prints the char at the top of the stack_s list if its in ascii range
+* and list is not empty.
+* @head: pointer to head pointer of list
+* @num: line number of opcode
+* Return: nothing
+**/
+void chr_top(stack_s **head, int num)
+{
+	if (!(*head || head))
+	{
+		printf("L<%d>: can't pchar, stack empty\n", num);
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n < 0 || (*head)->n > 127)
+	{
+		printf("L<%d>: can't pchar, value out of range\n", num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*head)->n);
+}
+
+/**
+* str_top - prints the string starting at the top of the stack_s list, if
+* its not empty and values are in ascii range.
+* @head: pointer to head pointer of list
+* @num: line number of opcode
+* Return: nothing
+**/
+void str_top(stack_s **head, int num)
+{
+	stack_s *tmp;
+
+	if (*head)
+	{
+		tmp = *head;
+		while (tmp && (tmp->n > 0 && tmp->n < 128))
+		{
+			printf("%c", tmp->n);
+			tmp = tmp->next;
+		}
+	}
+	printf("\n");
+}
